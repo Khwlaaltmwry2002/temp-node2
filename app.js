ed9f1d986224bb3863dd45f32a2be990aaa535b5
MohammedAltayeb92 <alttaib1992@gmail.com>
2024-12-24 16:22:08 +0300

    first commit

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const _ = require("lodash") // nameing convention for external modules to name t
 const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items) // flattenDeep is a method in lodash module(library) that flatten the array. run the program and see the result. it will be [1, 2, 3, 4]
 console.log(newItems)
+
+// git commands after creating your repo for pushing them into github
+// git remote add origin https://github.com/Khwlaaltmwry2002/temp-node2.git
+// git branch -M main
+// git push -u origin main
